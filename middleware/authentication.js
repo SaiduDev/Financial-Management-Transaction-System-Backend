d4fceafc7711559026f132fb0849d926b39f9846
Saidu M Koroma <saidumkoroma99@gmail.com>
2026-08-25 14:31:33 +0000

    auth Verification

---
+++ middleware/authentication.js
@@ -0,0 +1,25 @@
+import jwt from 'jsonwebtoken';
+import dotenv from "dotenv";
+
+dotenv.config();
+
+export const authVerify = (req, res, next)=>{
+    try {
+        let authHeader = req.headers.authorization;
+
+        if(!authHeader){
+            return res.status(404).json({message: "No Token provided"});
+        }
+
+        let token = authHeader.split(" ")[1];
+
+        let decoded = jwt.verify(token, process.env.JWT_Secret);
+
+        req.user = decode;
+
+        next();
+    } catch (error) {
+        res.status(500).json({message: "Token Expired, please login again"});
+        console.error(error);
+    }
+}
