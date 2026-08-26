dc1bd07ee69c8f5a36820b86d3e5a9580edf1ddc
Saidu M Koroma <saidumkoroma99@gmail.com>
2026-08-26 10:12:05 +0000

    admin authentication

--- controller/admin.js
+++ controller/admin.js
@@ -11,12 +11,12 @@ export const adminSignUp = async(req, res) => {
 
         let checkEmail = await pool.query("SELECT * FROM admin WHERE email = $1", [email]);
 
-        if(checkEmail.rows[0].length >= 1 ){
+        if(checkEmail.rows.length >= 1 ){
             return res.status(409).json({message: "Email address already existed"});
         }
 
         let hashedPassword = await bcrypt.hash(password, 10);
-        
+
         let newAdmin = await pool.query("INSERT INTO admin ( fullname, email, password ) VALUES($1, $2, $3) RETURNING *", [fullname, email, hashedPassword]);
 
         let token = jwt.sign(
@@ -35,4 +35,41 @@ export const adminSignUp = async(req, res) => {
         res.status(500).json({message: "failed to sign up new admin", error: error.message});
         console.error(error);
     }
+}
+
+export const adminLogIn  = async (req, res) => {
+try {
+    let { email , password } = req.body;
+
+    let verifyEmail = await pool.query("SELECT * FROM admin WHERE email = $1", [email]);
+
+    if(verifyEmail.rows.length === 0){
+        return res.status(409).json({message: "incorrect email or password"});
+    }
+
+    let admin = verifyEmail.rows[0];
+
+    let comparePassword = await bcrypt.compare(password, admin.password);
+
+    if(!comparePassword){
+        return res.status(401).json({message: "wrong credentials"});
+    }
+
+    let token = jwt.sign(
+        {id: admin.id, role: admin.role},
+        process.env.JWT_Secret,
+        {expiresIn: "4h"}
+    );
+
+    res.status(201).json(
+        {
+            success: true,
+            message: "Login successfully",
+            token
+        }
+    )
+} catch (error) {
+       res.status(500).json({message: "failed to log in admin", error: error.message});
+        console.error(error);
+}
 }
