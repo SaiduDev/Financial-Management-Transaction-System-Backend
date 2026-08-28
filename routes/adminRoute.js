251b6a28b6f925fe0423b2ae27eacc80c79cb4e7
Saidu M Koroma <saidumkoroma99@gmail.com>
2026-08-28 06:53:23 +0000

    admin code optimization

--- routes/adminRoute.js
+++ routes/adminRoute.js
@@ -2,12 +2,13 @@ import express from "express";
 import { adminLogIn, adminSignUp, adminProfile, changePassword } from "../controller/admin.js";
 import { authVerify } from "../middleware/authentication.js";
 import { authorize } from "../middleware/authorization.js";
+import { loginRateLimiter } from "../middleware/rateLimiter.js";
 
 const adminRouter  = express.Router();
 
 adminRouter.get("/admin/profile", authVerify, authorize("admin"), adminProfile);
 adminRouter.post("/auth/admin/signUp", adminSignUp);
-adminRouter.post("/auth/admin/logIn", adminLogIn);
+adminRouter.post("/auth/admin/logIn", loginRateLimiter, adminLogIn);
 adminRouter.post("/admin/changePassword", authVerify, authorize("admin"), changePassword);
 
 
